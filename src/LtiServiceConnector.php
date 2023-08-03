1ad6dce4e1a41da5a69d540b686fae1e8ef7d811
Brian Lin <lin.brian.l@gmail.com>
2023-08-03 10:59:46 -0500

    PODB-1260: Parse upper and lower-case header keys

--- src/LtiServiceConnector.php
+++ src/LtiServiceConnector.php
@@ -213,7 +213,7 @@ class LtiServiceConnector implements ILtiServiceConnector
 
     private function getNextUrl(array $headers)
     {
-        $subject = $headers['Link'] ?? '';
+        $subject = $headers['Link'] ?? $headers['link'] ?? '';
         preg_match(static::NEXT_PAGE_REGEX, $subject, $matches);
 
         return $matches[1] ?? null;
