2201519a754c16035ff2af3426e1c8502b8c0b13
Davo Hynds <dbhynds@gmail.com>
2023-10-20 15:50:29 -0500

    Add shouldMigrate to migration db interface

--- src/Interfaces/IMigrationDatabase.php
+++ src/Interfaces/IMigrationDatabase.php
@@ -20,6 +20,11 @@ interface IMigrationDatabase
     public function findLti1p1Keys(LtiMessageLaunch $launch): array;
 
     /**
+     * Given an LtiMessageLaunch, return true if this tool should migrate from 1.1 to 1.3
+     */
+    public function shouldMigrate(LtiMessageLaunch $launch): bool;
+
+    /**
      * This method should create a 1.3 deployment in your DB based on the LtiMessageLaunch.
      * Previous to this, we validated the oauth_consumer_key_sign to ensure this migration
      * can safely occur.
