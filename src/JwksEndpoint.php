efd1d627295020f2ea21ff76ccdec4761891ef3e
Davo Hynds <dbhynds@gmail.com>
2023-12-15 10:37:15 -0600

    Delete deprecated methods and classes

--- src/JwksEndpoint.php
+++ src/JwksEndpoint.php
@@ -50,14 +50,4 @@ class JwksEndpoint
 
         return ['keys' => $jwks];
     }
-
-    /**
-     * @deprecated
-     */
-    public function outputJwks()
-    {
-        trigger_error('Method '.__METHOD__.' is deprecated', E_USER_DEPRECATED);
-
-        echo json_encode($this->getPublicJwks());
-    }
 }
