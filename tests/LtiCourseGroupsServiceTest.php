d6463eef008c86cfe640a26e59bfc6bbfda1869a
Davo Hynds <dbhynds@gmail.com>
2023-12-20 09:46:25 -0600

    test group sets

--- tests/LtiCourseGroupsServiceTest.php
+++ tests/LtiCourseGroupsServiceTest.php
@@ -77,6 +77,86 @@ class LtiCourseGroupsServiceTest extends TestCase
         $this->assertEquals($expected, $result);
     }
 
+    public function testItGetGroupsBySet()
+    {
+        $serviceData = [
+            'context_groups_url' => 'https://example.com',
+            'context_group_sets_url' => 'https://example.com',
+            'scope' => ['asdf'],
+        ];
+
+        $service = new LtiCourseGroupsService($this->connector, $this->registration, $serviceData);
+
+        $groups = [
+            [
+                'id' => 'testId',
+                'set_id' => 'testSetId',
+            ],
+            [
+                'id' => 'testId2',
+                'set_id' => 'testSetId',
+            ],
+            [
+                'id' => 'testId3',
+                'set_id' => 'testSetId2',
+            ],
+            [
+                'id' => 'noSetId',
+            ],
+        ];
+        $sets = [
+            [
+                'id' => 'testSetId',
+            ],
+            [
+                'id' => 'testSetId2',
+            ],
+        ];
+
+        $expected = [
+            'testSetId' => [
+                'id' => 'testSetId',
+                'groups' => [
+                    'testId' => [
+                        'id' => 'testId',
+                        'set_id' => 'testSetId',
+                    ],
+                    'testId2' => [
+                        'id' => 'testId2',
+                        'set_id' => 'testSetId',
+                    ],
+                ],
+            ],
+            'testSetId2' => [
+                'id' => 'testSetId2',
+                'groups' => [
+                    'testId3' => [
+                        'id' => 'testId3',
+                        'set_id' => 'testSetId2',
+                    ],
+                ],
+            ],
+            'none' => [
+                'name' => 'None',
+                'id' => 'none',
+                'groups' => [
+                    'noSetId' => [
+                        'id' => 'noSetId',
+                    ],
+                ],
+            ],
+        ];
+
+        $this->connector->shouldReceive('getAll')
+            ->once()->andReturn($groups);
+        $this->connector->shouldReceive('getAll')
+            ->once()->andReturn($sets);
+
+        $result = $service->getGroupsBySet();
+
+        $this->assertEquals($expected, $result);
+    }
+
     public function testItGetsNoSetsForNoUrl()
     {
         $service = new LtiCourseGroupsService($this->connector, $this->registration, []);
